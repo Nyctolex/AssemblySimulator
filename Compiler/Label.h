e6b79036f2fb706c5cfb53cbe2074dda1acd468d
Alex Chen <nyctolex@gmail.com>
2022-04-02 12:55:10 +0300

    documentation and new functiosn
    Documentation was added in the header file.
    New function for line were added to support the location attribute.

--- Compiler/Label.h
+++ Compiler/Label.h
@@ -1,26 +1,87 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-#define TRUE 1
-#define FALSE 0
+#ifndef LABEL_H
+#define LABEL_H
+
+
+// char arr2[NUM_STRINGS][MAX_LENGTH] = { {"first string"},
+//                                           {"second string"},
+//                                           {"third string"},
+//                                           {"fourth string"},
+//                                           {"fifth string"} };
+/**
+ * @brief Label is a data struct of a linked list. It has the atrribute of a string name, 
+ * the location in the memory of which the Label is point to, and a pointer to the next node. 
+ */
 typedef struct Label
 {
     char name[50];      // Label's name
-    struct Label *next; // a pointer to the next label
-} label;
+    //location;**
+    int location; // the location of the matching line
+    struct Label *next; // a pointer to the next Label
+} Label;
 
 /**
- * @brief Gets a pointer to the head of the label list and it's name, and return the corresponding label.
+ * @brief returing the Label with the coresponding name.
  *
- * @param head
+ * @param head - the first node in the likned list.
  * @param name
  * @return Label
  */
-label *labelGetByName(label *head, char name[]);
-label *labelGetByIndex(label *head, int index);
-void labelAppendData(label *head, char name[]);
-void labelAppendNode(label *head, label *node);
-label *labelNewLabel(char name[]);
-label *labelLast(label *head);
-int labelListContains(label *head, char name[]);
+Label *labelGetByName(Label *head, char name[]);
+
+
+/**
+ * @brief Get the node in the corresponding index
+ * 
+ * @param head - the first node in the likned list.
+ * @param index 
+ * @return Label* (in the matching index)
+ */
+Label *labelGetByIndex(Label *head, int index);
+
+/**
+ * @brief Creating a new node with the given parameters and appending it to the end of the linked list.
+ * @param head - the first node in the likned list.
+ * @param name - the name of the new Label. 
+ * @param location - the memory location of which the Label is pointing to.
+ */
+void labelAppendData(Label *head, char name[], int location);
+
+
+/**
+ * @brief Append the given node to the end of the linked list.
+ * @param head - the first node in the likned list.
+ * @param node - the node to be appended.
+ */
+void labelAppendNode(Label *head, Label *node);
+
+
+/**
+ * @brief Creating a new Label node, and assigning it the given parameters.
+ * @param name - the name of the new Label
+ * @param location - the memory location of which the Label is pointing to.
+ * @return Label* - a pointer to the new node.
+ */
+Label *labelNewLabel(char name[], int location);
+
+/**
+ * @brief Getting the last node in the list.
+ * @param head - the first node in the likned list.
+ * @return Label* - the last label in the list.
+ */
+Label *labelLast(Label *head);
+
+/**
+ * @brief Return True(1) if the list contains a node with the corresponding name, else returns false(0).
+ * @param head - the first node in the likned list.
+ * @param name - the name to be searched for.
+ * @return int 
+ */
+int labelListContains(Label *head, char name[]);
+
+/**
+ * @brief Free a allocted memory of every node in the list. 
+ * @param head - the first node in the likned list.
+ */
+void labelDeleteList(Label *head);
 
 #endif
---
+++ Compiler/RegisterConst.h
@@ -0,0 +1,21 @@
+#ifndef REGHISTERCONST_H
+#define REGHISTERCONST  
+#define TRUE 1
+#define FALSE 0
+#define ZERO "0"
+#define IMM_REG "1"
+#define V0 "2"
+#define A0 "3"
+#define A1 "4"
+#define A2 "5"
+#define A3 "6"
+#define T0 "7"
+#define T1 "8"
+#define T2 "9"
+#define S0 "10"
+#define S1 "11"
+#define S2 "12"
+#define GP "13"
+#define SP "14"
+#define RA "15"
+#endif
