ba2f21d3e38de82ffbf36b83663b01d35b2c3ece
Horn <guyhorn5@gmail.com>
2022-04-07 22:06:29 +0300

    Main - Reached until check if imm in line
    Functions written:
    - iter_file
    - search_label
    - get_label
    - is_imm

--- Compiler/Main.c
+++ Compiler/Main.c
@@ -1,19 +1,73 @@
-#include<stdio.h>
+# include <stdio.h>
+# include <string.h>
+
+# define MAX_LINE_SIZE 501
+# define LABEL_ITER 0
+# define FINAL_ITER 1
+# define TRUE 1
+# define FALSE 0
+
+int is_imm(char *line)
+    {
+        for (int i = 0; i < MAX_LINE_SIZE; i++)
+            {
+                if (line[i] == 'i' && line[i+1] == 'm' && line[i+2] == 'm') return TRUE;
+            }
+        return FALSE;
+    }
+
+int get_label(char *line, int line_loc)
+    {
+        char label [MAX_LINE_SIZE] = "";
+        for (int i = 0; line[i] != ':'; i++) label[i] = line[i];
+        // printf("%s %d\n", label, line_loc);
+    }
+
+int search_label(char *line, int line_index, int line_loc) // iterate a line and check if there is a label in it
+    {
+        for (int i = 0; i < MAX_LINE_SIZE; i++)
+            {
+                if (line[i] == '\0') break;
+                if (line[i] == ':') 
+                    {
+                        get_label(line, line_loc);
+                        return line_loc;
+                    }
+            }
+        if (is_imm(line)) line_loc++;
+        printf("%d, %d, %d: %s", line_index, line_loc, line_loc-line_index, line);
+        return line_loc;
+    }
+
+int translate_file(char *line, int line_index, int line_loc)
+    {
+        
+    }
+
+int iter_file(FILE *fp, char iter_type)
+    {
+        FILE *asm_file = fp;
+        char line [MAX_LINE_SIZE];
+        int line_index = 0;
+        int line_loc = 0;
+
+        while (fgets(line, MAX_LINE_SIZE, asm_file))
+            {
+                if (iter_type == LABEL_ITER) line_loc = search_label(line, line_index, line_loc);
+                else if (iter_type == FINAL_ITER) translate_file(line, line_index, line_loc);
+                line_index++;
+                line_loc++;
+            } 
+    }
 
 int main()
     {
-        FILE *memin = NULL;
-        char num;
-        memin = fopen("Fibonacci\\fib.asm", "r");
-        if (memin)
+        FILE *asm_file = fopen("Fibonacci\\fib.asm", "r"); // ! dont forget to change file name
+        
+        if (asm_file)
             {
-                num = fgetc(memin);
-                while(num != EOF)
-                    {
-                        printf("%c", num);
-                        num = fgetc(memin);
-                    }
+                iter_file(asm_file, LABEL_ITER);
             }
-        fclose(memin);
+        fclose(asm_file);
         return 0;
     }
--- Compiler/tempCodeRunnerFile.c
+++ Compiler/tempCodeRunnerFile.c
@@ -1,2 +1,1 @@
-ILE *memin;
-        m
+%d, 
