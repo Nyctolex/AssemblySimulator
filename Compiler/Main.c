c689538f6a55afcd793bedd426560098363e6beb
Horn <guyhorn5@gmail.com>
2022-04-15 18:36:32 +0300

    Added dict

--- Compiler/Main.c
+++ Compiler/Main.c
@@ -1,8 +1,10 @@
 # include <stdio.h>
 # include <string.h>
+# include "Label.h"
+# include "dict.c"
 
 # define MAX_LINE_SIZE 501
-# define LABEL_ITER 0
+# define GET_LABEL 0
 # define FINAL_ITER 1
 # define TRUE 1
 # define FALSE 0
@@ -15,36 +17,56 @@ int is_imm(char *line)
             }
         return FALSE;
     }
-
-int get_label(char *line, int line_loc)
+int add_label(char *line, int line_loc)
     {
         char label [MAX_LINE_SIZE] = "";
         for (int i = 0; line[i] != ':'; i++) label[i] = line[i];
-        // printf("%s %d\n", label, line_loc);
+        // ! append label
     }
-
 int search_label(char *line, int line_index, int line_loc) // iterate a line and check if there is a label in it
     {
         for (int i = 0; i < MAX_LINE_SIZE; i++)
             {
                 if (line[i] == '\0') break;
-                if (line[i] == ':') 
+                if (line[i] == ':')
                     {
-                        get_label(line, line_loc);
+                        add_label(line, line_loc);
                         return line_loc;
                     }
             }
         if (is_imm(line)) line_loc++;
-        printf("%d, %d, %d: %s", line_index, line_loc, line_loc-line_index, line);
+        // printf("%d, %d, %d: %s", line_index, line_loc, line_loc-line_index, line);
         return line_loc;
     }
-
 int translate_file(char *line, int line_index, int line_loc)
     {
-        
+        char *var = "";
+        int counter = 0;
+        int hex;
+        for (int i = 0; i < MAX_LINE_SIZE; i++) // check if line is only label
+            {
+                if (line[i] == '\0') break;
+                if (line[i] == ':')
+                    {
+                        // printf("%s", line);
+                        // ! search the label's val and output it
+                    }
+            }
+        for (int i = 0; i < MAX_LINE_SIZE; i++)
+            {
+                if (line[i] == '\0' || line[i] == '#') break;
+                else if (line[i] == ' ') continue;
+                else if (line[i] == ',')
+                    {
+                        hex = compare(var);
+                    }
+                else
+                    {
+                        strcat(var, line[i]); // FIXME
+                    }
+            }
     }
-
-int iter_file(FILE *fp, char iter_type)
+int iter_lines(FILE *fp, char iter_type)
     {
         FILE *asm_file = fp;
         char line [MAX_LINE_SIZE];
@@ -53,20 +75,20 @@ int iter_file(FILE *fp, char iter_type)
 
         while (fgets(line, MAX_LINE_SIZE, asm_file))
             {
-                if (iter_type == LABEL_ITER) line_loc = search_label(line, line_index, line_loc);
+                if (iter_type == GET_LABEL) line_loc = search_label(line, line_index, line_loc);
                 else if (iter_type == FINAL_ITER) translate_file(line, line_index, line_loc);
                 line_index++;
                 line_loc++;
             } 
     }
-
 int main()
     {
         FILE *asm_file = fopen("Fibonacci\\fib.asm", "r"); // ! dont forget to change file name
-        
         if (asm_file)
             {
-                iter_file(asm_file, LABEL_ITER);
+                iter_lines(asm_file, GET_LABEL);
+                fseek(asm_file, 0, 0);
+                iter_lines(asm_file, FINAL_ITER);
             }
         fclose(asm_file);
         return 0;
---
+++ Compiler/dict.c
@@ -0,0 +1,86 @@
+# include <string.h>
+// * Creating Strings
+    // * Registers
+        char zero[] = "$zero";
+        char imm[] = "$imm";
+        char v0[] = "$v0";
+        char a0[] = "$a0";
+        char a1[] = "$a1";
+        char a2[] = "$a2";
+        char a3[] = "$a3";
+        char t0[] = "$t0";
+        char t1[] = "$t1";
+        char t2[] = "$t2";
+        char s0[] = "$s0";
+        char s1[] = "$s1";
+        char s2[] = "$s2";
+        char gp[] = "$gp";
+        char sp[] = "$sp";
+        char ra[] = "$ra";
+    // * OpCodes
+        char add[] = "$add";
+        char sub[] = "$sub";
+        char mul[] = "$mul";
+        char and[] = "$and";
+        char or[] = "$or";
+        char xor[] = "$xor";
+        char sll[] = "$sll";
+        char sra[] = "$sra";
+        char srl[] = "$srl";
+        char beq[] = "$beq";
+        char bne[] = "$bne";
+        char blt[] = "$blt";
+        char bgt[] = "$bgt";
+        char ble[] = "$ble";
+        char bge[] = "$bge";
+        char jal[] = "$jal";
+        char lw[] = "$lw";
+        char sw[] = "$sw";
+        char reti[] = "$reti";
+        char in[] = "$in";
+        char out[] = "$out";
+        char halt[] = "$halt";
+// * Compare Strings
+    int compare(char *exp) // returns the expression's opcode
+        {
+            // * Registers
+                if (!strcmp(exp, zero)) return 0;
+                else if (!strcmp(exp, imm)) return 1;
+                else if (!strcmp(exp, v0)) return 2;
+                else if (!strcmp(exp, a0)) return 3;
+                else if (!strcmp(exp, a1)) return 4;
+                else if (!strcmp(exp, a2)) return 5;
+                else if (!strcmp(exp, a3)) return 6;
+                else if (!strcmp(exp, t0)) return 7;
+                else if (!strcmp(exp, t1)) return 8;
+                else if (!strcmp(exp, t2)) return 9;
+                else if (!strcmp(exp, s0)) return 10;
+                else if (!strcmp(exp, s1)) return 11;
+                else if (!strcmp(exp, s2)) return 12;
+                else if (!strcmp(exp, gp)) return 13;
+                else if (!strcmp(exp, sp)) return 14;
+                else if (!strcmp(exp, ra)) return 15;
+            // * OpCodes
+                else if (!strcmp(exp, add)) return 0;
+                else if (!strcmp(exp, sub)) return 1;
+                else if (!strcmp(exp, mul)) return 2;
+                else if (!strcmp(exp, and)) return 3;
+                else if (!strcmp(exp, or)) return 4;
+                else if (!strcmp(exp, xor)) return 5;
+                else if (!strcmp(exp, sll)) return 6;
+                else if (!strcmp(exp, sra)) return 7;
+                else if (!strcmp(exp, srl)) return 8;
+                else if (!strcmp(exp, beq)) return 9;
+                else if (!strcmp(exp, bne)) return 10;
+                else if (!strcmp(exp, blt)) return 11;
+                else if (!strcmp(exp, bgt)) return 12;
+                else if (!strcmp(exp, ble)) return 13;
+                else if (!strcmp(exp, bge)) return 14;
+                else if (!strcmp(exp, jal)) return 15;
+                else if (!strcmp(exp, lw)) return 16;
+                else if (!strcmp(exp, sw)) return 17;
+                else if (!strcmp(exp, reti)) return 18;
+                else if (!strcmp(exp, in)) return 19;
+                else if (!strcmp(exp, out)) return 20;
+                else if (!strcmp(exp, halt)) return 21;
+        }
