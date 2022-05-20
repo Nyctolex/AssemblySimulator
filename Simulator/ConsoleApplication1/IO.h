ac6360b1a8db93b4d78ce2fd724ec65c2e0301cf
Horn <guyhorn5@gmail.com>
2022-05-20 17:02:37 +0300

    Update IO.h

--- Simulator/ConsoleApplication1/IO.h
+++ Simulator/ConsoleApplication1/IO.h
@@ -6,6 +6,6 @@
     int timer(int ioreg[]);
     int irq(int ioreg[], int *pc, int is_task);
     int check_interrupts(int ioreg[], int *pc, int *is_task, int irq2[]);
-    int add_irq2(int *irq2in);
+    void add_irq2(FILE *irq2in, int *irq2)
 # endif
 // ! add comments
