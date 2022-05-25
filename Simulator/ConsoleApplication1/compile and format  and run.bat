fba02b0b27d4679e58bc4604af89dd0f80d8abee
Alex Chen <nycolex@gmail.com>
2022-05-25 11:22:36 +0300

    compile and run

---
+++ Simulator/ConsoleApplication1/compile and format  and run.bat
@@ -0,0 +1,4 @@
+python reformat.py
+Main.exe test.asm memin.txt
+..\x64\Debug\ConsoleApplication1.exe memin.txt diskin.txt irq2in.txt memout.txt regout.txt trace.txt hwregtrace.txt cycles.txt leds.txt display7seg.txt diskout.txt monitor.txt monitor.yuv
+PAUSE
