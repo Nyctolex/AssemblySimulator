c3426e12e66237ec8ce6c90f472f39069ab27486
Horn <guyhorn5@gmail.com>
2022-05-28 12:15:17 +0300

    IO - Changed reg names

--- Simulator/ConsoleApplication1/IO.c
+++ Simulator/ConsoleApplication1/IO.c
@@ -52,3 +52,93 @@ void monitor(int monitor_arr[], int ioreg[])
                 ioreg[22] = 0;
             }
     }
+<<<<<<< Updated upstream
+=======
+void disk_command(int ioreg[], char disk_memory[][MAX_DISK_LINE], int *disk_cycle, char memory[LINES_MAX][LINES_MAX_SIZE])
+    {
+        int is_full = 0;
+        int line_index = -1;
+        if ((ioreg[diskcmd] != 0) && (disk_cycle == 0)) // if there is a disk cmd
+            {
+                disk_cycle = 1024;
+                if (ioreg[diskstatus] == 0) // if disk is not busy
+                    {
+                        if (ioreg[diskcmd] == 1) // read sector
+                            {
+                                for (int i=1000; i<LINES_MAX; i++) // for every memory line greater than 1000
+                                    {
+                                        is_full = 0;
+                                        for (int j=0; j<LINES_MAX_SIZE) // for every element in memory line
+                                            {
+                                                if (memory[i][j] != 0) // check if an element is occupied
+                                                    {
+                                                        is_full = 1;
+                                                        break;
+                                                    }
+                                            }
+                                        if (is_full == 1) continue;
+                                        else if (is_full == 1) line_index = i;
+                                    }
+                                strcopy(memory[line_index], disk_memory[disksector*SECTOR_SIZE+diskbuffer]);
+                            }
+                        else if (ioreg[diskcmd] == 2) // write sector
+                            {
+                                strcopy(disk_memory[disksector*SECTOR_SIZE+diskbuffer], memory[line_index]);
+                            }
+                    }
+            }
+        if ((disk_cycle != 1) && (disk_cycle != 1024)) disk_cycle--;
+        if (disk_cycle == 1)
+            {
+                ioreg[diskcmd] = 0;
+                ioreg[diskstatus] = 0;
+                ioreg[irq1status] = 1;
+                disk_cycle--;
+            }
+    }
+void led_write(int ioreg[], int *led, FILE *leds_file, int *pc)
+    {
+        char line_str[] = {0};
+        if (ioreg[leds] != led)
+            {
+                led = ioreg[leds];
+                sprintf(line_str, "%d %08X\n", pc, atoi(led));
+                fputs(line_str, leds_file);
+            }
+    }
+void hwregtrace_write(FILE *fp, int *pc, int read_write, int reg_num, int data)
+    {
+        char line_str[] = {0};
+        char action[6] = {0};
+        char reg_name[5] = {0};
+        if (read_write == 0) sprintf(action, "READ");
+        else if (read_write == 1) sprintf(action, "WRITE");
+        switch(reg_num)
+            {
+                case zero_reg: sprintf(reg_name, "zero");
+                case imm_reg: sprintf(reg_name, "imm");
+                case v0_reg: sprintf(reg_name, "v0");
+                case a0_reg: sprintf(reg_name, "a0");
+                case a1_reg: sprintf(reg_name, "a1");
+                case a2_reg: sprintf(reg_name, "a2");
+                case a3_reg: sprintf(reg_name, "a3");
+                case t0_reg: sprintf(reg_name, "t0");
+                case t1_reg: sprintf(reg_name, "t1");
+                case t2_reg: sprintf(reg_name, "t2");
+                case s0_reg: sprintf(reg_name, "s0");
+                case s1_reg: sprintf(reg_name, "s1");
+                case s2_reg: sprintf(reg_name, "s2");
+                case gp_reg: sprintf(reg_name, "gp");
+                case sp_reg: sprintf(reg_name, "sp");
+                case ra_reg: sprintf(reg_name, "ra");
+            }
+        sprintf(line_str, "%d %s %s %08X\n", pc, action, reg_name, data);
+        fputs(line_str, fp);
+    }
+void display7seg_write(FILE *display7seg_file, int ioreg[], int *pc)
+    {
+        char line_str[] = {0};
+        sprintf(line_str, "%d %08X", pc, ioreg[display7seg]);
+        fputs(line_str, display7seg_file);
+    }
+>>>>>>> Stashed changes
--- Simulator/ConsoleApplication1/IO.h
+++ Simulator/ConsoleApplication1/IO.h
@@ -9,6 +9,52 @@
 # define MAX_DISK_LINE 10
 # define SECTOR_AMOUNT 128
 # define SECTOR_SIZE 128
+<<<<<<< Updated upstream
+=======
+# define LINES_MAX 4096 // ! make sure
+# define LINES_MAX_SIZE 10 // ! make sure
+
+    #define zero_reg 0
+	#define imm_reg  1
+	#define v0_reg  2
+	#define a0_reg  3
+	#define a1_reg  4
+	#define a2_reg  5
+	#define a3_reg  6
+	#define t0_reg  7
+	#define t1_reg  8
+	#define t2_reg  9
+	#define s0_reg  10
+	#define s1_reg  11
+	#define s2_reg  12
+	#define gp_reg  13
+	#define sp_reg  14
+	#define ra_reg  15
+    
+    # define irq0enable 0
+    # define irq1enable 1
+    # define irq2enable 2
+    # define irq0status 3
+    # define irq1status 4
+    # define irq2status 5
+    # define irqhandler 6
+    # define irqreturn 7
+    # define clks 8
+    # define leds 9
+    # define display7seg 10
+    # define timerenable 11
+    # define timercurrent 12
+    # define timermax 13
+    # define diskcmd 14
+    # define disksector 15
+    # define diskbuffer 16
+    # define diskstatus 17
+    # define reserved 18
+    # define reserved 19
+    # define monitoraddr 20
+    # define monitordata 21
+    # define monitorcmd 22
+>>>>>>> Stashed changes
 
 int timer(int ioreg[]);
 int irq(int ioreg[], int* pc, int is_task);
