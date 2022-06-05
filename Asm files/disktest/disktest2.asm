f269a472135862760843b46fa14229b5caa93716
Horn <guyhorn5@gmail.com>
2022-06-05 23:56:14 +0300

    Disktest - Update

--- Asm files/disktest/disktest2.asm
+++ Asm files/disktest/disktest2.asm
@@ -1,6 +1,6 @@
 add $t0, $zero, $zero, 0
 add $a1, $zero, $zero, 0
-add $s1, $t0, $imm, 1000
+add $s1, $zero, $imm, 1000
 add $s2, $zero, $imm, 2000
 LoopStart:
     add $t1, $zero, $imm, LoopStart
@@ -13,10 +13,21 @@ LoopStart:
     out $s1, $zero, $imm, 16                    # diskbuffer = 1000
     add $t1, $zero, $imm, 1
     out $t1, $zero, $imm, 14                    # diskcmd = read
-    lw $a0, $zero, $s1, 0                       # a0 = mem[1000]
-    lw $a1, $zero, $s2, 0                       # a1 = mem[2000]
+IsBusy:
+    add $t1, $zero, $imm, IsBusy
+    in $t2, $zero, $imm, 17
+    bne $t1, $zero, $t2, 0                      # check if disk is busy
+    add $t2, $zero, $imm, 128
+for128:
+    lw $a0, $zero, $s1, 0                       # a0 = mem[1000+j]
+    add $s1, $s1, $imm, 1
+    lw $a1, $zero, $s2, 0                       # a1 = mem[2000+j]
     add $a1, $a1, $a0, 0                        # a1 = a0 + a1
-    sw $a1, $s2, $zero, 0                       # mem[2000] = a1
+    sw $a1, $s2, $zero, 0                       # mem[2000+j] = a1
+    add $s2, $s2, $imm, 1
+    sub $t2, $t2, $imm, 1
+    add $t1, $zero, $imm, for128
+    bne $t1, $zero, $t2, 0
     add $t0, $t0, $imm, 1                       # i++
     add $t1, $zero, $imm, LoopStart             # return to LoopStart
     beq $t1, $zero, $zero, 0
@@ -26,6 +37,7 @@ LoopEnd:
     bne $t1, $zero, $t2, 0                      # check if disk is busy
     add $t1, $zero, $imm, 8                     # write to sec[8]
     out $t1, $zero, $imm, 15                    # disksector = 8
+    add $s2, $zero, $imm, 2000
     out $s2, $zero, $imm, 16                    # diskbuffer = 2000
     add $t1, $zero, $imm, 2
     out $t1, $zero, $imm, 14                    # diskcmd = write
